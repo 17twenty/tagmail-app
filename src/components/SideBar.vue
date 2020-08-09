71f9efc004945bac947c85175768f65ca49ac64e
Mauricio Riquelme <m.riquelme223@gmail.com>
2020-08-09 14:14:59 +1000

    Update src/components/SideBar.vue
    Co-authored-by: Nick Glynn <n.s.glynn@gmail.com>

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -4,7 +4,7 @@
     <div class="sidebar-items">
       <router-link to="/dashboard/email-designs">Email Templates</router-link>
       <router-link to="/sending-preferences">Sending Preferences</router-link>
-      <router-link to="/dashboard/theme-logo-branding">Theme / Logo / Branding</router-link>
+      <router-link to="/dashboard/theme-logo-branding">Project Theme</router-link>
       <router-link to="/stats-and-metrics">Stats and Metrics</router-link>
     </div>
     <div class="sidebar-end">
