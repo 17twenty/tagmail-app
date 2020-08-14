0848863ee574036268730b40a475ac77d9da572b
Nick Glynn <nick.glynn@versent.com.au>
2020-08-14 13:49:31 +1000

    Payload fix

--- src/components/FormElementText.vue
+++ src/components/FormElementText.vue
@@ -5,12 +5,12 @@
     </b-field>
     <b-field label="Text Weight">
       <b-select expanded v-model.trim="form.weight" placeholder="weight...">
-        <option value="muted">Muted</option>
-        <option value="title">Title</option>
-        <option selected value="default">Default</option>
+        <option value="mutedText">Muted</option>
+        <option value="titleText">Title</option>
+        <option selected value="defaultText">Default</option>
       </b-select>
     </b-field>
-    <b-field label="Text Position">
+    <b-field label="Position">
       <b-select expanded v-model.trim="form.position" placeholder="position...">
         <option value="left">Left</option>
         <option value="right">Right</option>
