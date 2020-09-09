56120eb3e8316aa400143d061e146ae22c5f8645
mauricio riquelme <m.riquelme223@gmail.com>
2020-09-09 16:35:15 +1000

    heavy boi

--- src/components/FormElementText.vue
+++ src/components/FormElementText.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <b-field label="Text">
-      <b-input type="textarea" v-model="form.text" :value="form.text" />
+      <Editor
+        :options="editorConfig"
+        ref="toastuiEditor"
+        :initialValue="form.text"
+        initialEditType="wysiwyg"
+        @change="handleChange"
+      />
     </b-field>
     <b-field label="Text Weight">
       <b-select expanded v-model.trim="form.weight" placeholder="weight...">
@@ -21,12 +27,61 @@
 </template>
 
 <script>
+/* eslint-disable import/no-extraneous-dependencies */
+
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import { Editor } from '@toast-ui/vue-editor';
+
 export default {
   name: 'FormElementText',
   props: {
     form: {
       type: Object,
       required: true,
+    },
+  },
+  components: {
+    Editor,
+  },
+  data() {
+    return {
+      editorConfig: {
+        language: 'en-US',
+        useCommandShortcut: true,
+        useDefaultHTMLSanitizer: true,
+        usageStatistics: false,
+        hideModeSwitch: true,
+        toolbarItems: [
+          'heading',
+          'bold',
+          'italic',
+          'strike',
+          'divider',
+          'hr',
+          'quote',
+          'divider',
+          'ul',
+          'ol',
+          'task',
+          'indent',
+          'outdent',
+          'divider',
+          'table',
+          'image',
+          'link',
+          'divider',
+          'code',
+          'codeblock',
+        ],
+      },
+    };
+  },
+  methods: {
+    handleChange() {
+      const markdown = this.$refs.toastuiEditor.invoke('getMarkdown');
+      this.$set(this.form, 'text', markdown);
     },
   },
 };
